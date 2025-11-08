c059f65f97c11b734b3350b1e1b761bef5475d27
Phong Nguyen <nphong2103@gmail.com>
2025-11-08 11:19:24 +0700

    Add version

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "version.h"
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello World" << std::endl;
+    std::cout << APP_NAME << " v" << APP_VERSION << std::endl;
+    std::cout << APP_DESCRIPTION << std::endl;
     return 0;
 }
