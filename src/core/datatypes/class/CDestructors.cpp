ed155440a796d6d9880ec8ac45d9e6bbf655b53b
Phong Nguyen <nphong2103@gmail.com>
2025-11-10 13:03:50 +0700

    Add core/datatypes/class constructers/destructors

---
+++ src/core/datatypes/class/CDestructors.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+// *1. Basic Destructor
+namespace Basic
+{
+    class CDestructors
+    {
+    public:
+        CDestructors()
+        {
+            cout << "Called CDestructors() \n";
+        }
+
+        ~CDestructors()
+        {
+            cout << "Called ~CDestructors() \n";
+        }
+
+        // Using `default` keyword
+        // ~CDestructors() = default;
+
+        // Using `delete` // I forbid this destructor
+        // ~CDestructors() = delete;
+    };
+
+    void destructers()
+    {
+        cout << "\n--- Basic Destructer Examples ---\n";
+        {
+            CDestructors obj;
+        }
+    }
+}
+
+// *2. Virtual Destructor
+namespace Virtual
+{
+    class CDestructorsBase // final => cannot inherit
+    {
+    public:
+        CDestructorsBase()
+        {
+            cout << "Called CDestructorsBase() \n";
+        }
+
+        virtual ~CDestructorsBase()
+        {
+            cout << "Called ~CDestructorsBase() \n";
+        }
+
+        // Using `default` keyword
+        // ~CDestructorsBase() = default;
+    };
+
+    class CDestructorsDerived : public CDestructorsBase
+    {
+    public:
+        CDestructorsDerived()
+        {
+            cout << "Called CDestructorsDerived() \n";
+        }
+
+        ~CDestructorsDerived() override
+        {
+            cout << "Called ~CDestructorsDerived() \n";
+        }
+    };
+
+    void destructers()
+    {
+        cout << "\n--- Virtual Destructer Examples ---\n";
+        CDestructorsDerived *derived = {new CDestructorsDerived()};
+        CDestructorsBase *base{derived};
+        delete base;
+        // without virtual -> only call ~CDestructorsBase()
+        // with virtual -> call ~CDestructorsBase() && ~CDestructorsDerived()
+    }
+}
+
+struct CDestructorsAutoRuner
+{
+    CDestructorsAutoRuner()
+    {
+        Basic::destructers();
+        Virtual::destructers();
+    }
+};
+
+static CDestructorsAutoRuner instance;
